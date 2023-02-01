604ff0eac1f3d41a92891a3269991e3af688e390
rizal <rizal.firdaus117@gmail.com>
2023-02-01 15:36:39 +0700

    Processor Test

--- tests/ProcessorTest.php
+++ tests/ProcessorTest.php
@@ -4,6 +4,9 @@ namespace Rizal\Logger;
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Monolog\Processor\HostnameProcessor;
+use Monolog\Processor\MemoryUsageProcessor;
+use Monolog\Processor\WebProcessor;
 use PHPUnit\Framework\TestCase;
 
 class ProcessorTest extends TestCase
@@ -12,6 +15,9 @@ class ProcessorTest extends TestCase
     {
         $logger = new Logger(ProcessorTest::class);
         $logger->pushHandler(new StreamHandler(__DIR__ . "/../application.log"));
+        $logger->pushProcessor(new MemoryUsageProcessor());
+        $logger->pushProcessor(new HostnameProcessor());
+        $logger->pushProcessor(new WebProcessor());
         $logger->pushProcessor(function ($record) {
             $record["extra"]["app"] = [
                 "author" => "Rizal",
