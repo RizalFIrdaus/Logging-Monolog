f646cee552355e5a39a1d2faf79cbfbe78c79da9
rizal <rizal.firdaus117@gmail.com>
2023-02-01 14:47:38 +0700

    Level Logging monolog

---
+++ tests/LevelTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rizal\Logger;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+
+class LevelTest extends TestCase
+{
+    public function testLevel()
+    {
+        $logger = new Logger(LevelTest::class);
+        $logger->pushHandler(new StreamHandler(__DIR__ . "/../application.log"));
+        $logger->info("Info Level");
+        $logger->emergency("Emergency Level");
+        $logger->debug("Debug Level");
+        $logger->alert("Alert Level");
+        $logger->critical("Critical Level");
+        $logger->notice("Notice Level");
+        self::assertNotNull($logger);
+    }
+}
